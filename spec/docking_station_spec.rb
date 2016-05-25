05099f5a70eecab91afcdfdb6cbeb0c5e2ee5c34
Rb2030 <rossmail2050@gmail.com>
2016-05-25 13:19:35 +0100

    Created release bike function

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,8 @@
 require 'docking_station'
 
 describe DockingStation do
-
+  # it 'responds to release bike' do
+  #   expect(DockingStation.new).to respond_to :release_bike
+  # end
+  it {is_expected.to respond_to :release_bike}
 end
