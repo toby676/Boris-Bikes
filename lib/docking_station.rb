465915261fbbead134aef3eedf23e914855e5118
Rb2030 <rossmail2050@gmail.com>
2016-05-25 15:00:53 +0100

    Finish docking process

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,15 @@
 
 class DockingStation
+  attr_accessor :bike
   def release_bike
     Bike.new
   end
 
-  def dock
-
+  def dock(bike)
+    @bike = bike
   end
-  
-  def bike
-
-  end
+  # replaced by attr_accessor
+  # def bike
+  #   @bike
+  # end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   # it 'responds to release bike' do
@@ -9,12 +10,21 @@ describe DockingStation do
     #expect(DockingStation.new).to
     docking_station = DockingStation.new
     bike = docking_station.release_bike
-    expect(bike).to be_working  
+    expect(bike).to be_working
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument }
   it {is_expected.to respond_to :bike }
   it 'allows a bike to dock' do
-    
+    station = DockingStation.new
+    bike = Bike.new
+    expect(station.dock(bike)).to eq(bike)
+  end
+
+  it 'allows to see a docked bike' do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    expect(station.bike).to eq(bike)
   end
 end
