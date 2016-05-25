c7d278624ebcb3f49cb1e40834c2e37927430aeb
riyapabari <riya.pabari@gmail.com>
2016-05-25 18:02:24 +0100

    Completed challenge 12

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,15 +1,16 @@
 require_relative 'Bike'
 
 class DockingStation
-	
+
   attr_reader :bike
 
 	def release_bike
-		Bike.new
+	  raise "No bikes available!" unless @bike
+	  @bike
 	end
 
 	def dock(bike)
-		@bike = bike 
+	  @bike = bike
 	end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -2,27 +2,43 @@ require 'DockingStation'
 
 describe DockingStation do
 	
-  it { is_expected.to respond_to :release_bike }
+  it {is_expected.to respond_to :release_bike}
 
-	it 'releases working bikes' do
-		bike = subject.release_bike
-	  	expect(bike).to be_working
+  describe '#release_bike' do
+
+    it 'releases a bike if bikes are available' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'raises an error if no bikes available' do
+      expect{subject.release_bike}.to raise_error("No bikes available!")
+    end
+
+  end
+
+  it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    subject.release_bike
+	  expect(bike).to be_working
 	end
     
-  it { is_expected.to respond_to(:dock).with(1).argument }
+  it {is_expected.to respond_to(:dock).with(1).argument}
 
-  it { is_expected.to respond_to (:bike) }
+  it {is_expected.to respond_to (:bike)}
 
-  it 'docks something' do 
+  it 'docks the bike' do 
   		bike = Bike.new
-  		expect(subject.dock(bike)).to eq bike 
+  		expect(subject.dock(bike)).to eq bike
   end 
 
-  it 'reports a bike' do 
+  it 'returns docked bike' do 
   	bike = Bike.new
   	subject.dock(bike)
   	expect(subject.bike).to eq bike
-  end 
+  end
 
 end
 
