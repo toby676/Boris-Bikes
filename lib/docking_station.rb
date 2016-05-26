916565d9f44ed5e71afae3c0c0538e95aa3dc494
Hassan Radheyyan <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-05-26 12:13:32 +0100

    Refactored Code to Use Single Responsibility Principle

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,17 +5,25 @@ class DockingStation
   end
 
   def release_bike
-    raise "No Bikes" if (@bike.length == 0)
+    raise "No Bikes" if empty?
     @bike.pop
 
   end
 
   def dock(bike)
-    raise "Is Full" if @bike.length >= 20
+    raise "Is Full" if full?
     @bike.push(bike)
   end
-  # replaced by attr_accessor
-  # def bike
-  #   @bike
-  # end
+
+  private
+
+  def full?
+    return true if @bike.length >= 20
+    false
+  end
+
+  def empty?
+    return true if @bike.length == 0
+    false
+  end
 end
